52e81c5dbf9359326fb4f0e8b31f794dae7518c6
Júlia Martins Monteiro <juliamamonteiro02@gmail.com>
2025-04-21 11:06:48 -0300

    Create professorRoutes.js

---
+++ backend/routes/professorRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const { createProfessor, getProfessores, login, updateProfessor, deleteProfessor, getProfessorById } = require('../controllers/professorController');
+
+router.post('/', createProfessor);
+router.get('/', getProfessores);
+router.get('/:id', getProfessorById);
+router.post('/login', login);
+router.put('/:id', updateProfessor); 
+router.delete('/:id', deleteProfessor); 
+
+module.exports = router;
